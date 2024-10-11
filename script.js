22f2b18d3581a1f3a9fd9d0427514826bf083be8
yoruhiki <hoangedm2000@gmail.com>
2024-10-11 22:08:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -38,4 +38,15 @@ document.addEventListener("DOMContentLoaded", function () {
   if (savedBackgroundImage) {
     document.body.style.backgroundImage = `url(${savedBackgroundImage})`;
   }
+
+  function updateTime() {
+    const vietnamTime = new Date().toLocaleString("en-US", { timeZone: "Asia/Ho_Chi_Minh" });
+    const tokyoTime = new Date().toLocaleString("en-US", { timeZone: "Asia/Tokyo" });
+
+    document.getElementById("vietnam-time").textContent = `Vietnam Time: ${vietnamTime}`;
+    document.getElementById("tokyo-time").textContent = `Tokyo Time: ${tokyoTime}`;
+  }
+
+  setInterval(updateTime, 1000);
+  updateTime();
 });
